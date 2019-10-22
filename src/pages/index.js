4061d9fab7f88b117a20f7b802b1eb85a0968da3
Jull Weber <mkykode@gmail.com>
2019-10-22 12:55:25 -0700

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+   <form>
+     Form here
+   </form>
+  </Layout>
+)
+
+export default IndexPage
