590c2952b6754742a998fb740822a831057f37eb
Jull Weber <mkykode@gmail.com>
2019-10-22 20:36:38 -0700

    add recaptcha

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,18 @@
 import React from "react"
 import { Link, navigate } from "gatsby"
-
+import Recaptcha from "react-google-recaptcha"
 import Layout from "../components/layout"
 // import Image from "../components/image"
 import SEO from "../components/seo"
-
+const RECAPTCHA_KEY = process.env.GATSBY_APP_SITE_RECAPTCHA_KEY
+// if (typeof RECAPTCHA_KEY === "undefined") {
+//   throw new Error(`
+//   Env var GATSBY_APP_SITE_RECAPTCHA_KEY is undefined!
+//   You probably forget to set it in your Netlify build environment variables.
+//   Make sure to get a Recaptcha key at https://www.netlify.com/docs/form-handling/#custom-recaptcha-2-with-your-own-settings
+//   Note this demo is specifically for Recaptcha v2
+//   `)
+// }
 function encode(data) {
   return Object.keys(data)
     .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
@@ -12,6 +20,7 @@ function encode(data) {
 }
 const Contact = () => {
   const [state, setState] = React.useState({})
+  const recaptchaRef = React.createRef()
 
   const handleChange = e => {
     setState({ ...state, [e.target.name]: e.target.value })
@@ -20,11 +29,13 @@ const Contact = () => {
   const handleSubmit = e => {
     e.preventDefault()
     const form = e.target
+    const recaptchaValue = recaptchaRef.current.getValue()
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({
         "form-name": form.getAttribute("name"),
+        "g-recaptcha-response": recaptchaValue,
         ...state,
       }),
     })
@@ -73,7 +84,7 @@ const Contact = () => {
             <textarea name="message" onChange={handleChange} />
           </label>
         </p>
-        <div data-netlify-recaptcha="true"></div>
+        <Recaptcha ref={recaptchaRef} sitekey={RECAPTCHA_KEY} />
         <p>
           <button type="submit">Send</button>
         </p>
