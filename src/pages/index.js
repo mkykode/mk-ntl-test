fcc7ae53dfbdc1da9f95700062f1a6261c1da7f9
Jull Weber <mkykode@gmail.com>
2019-10-22 19:41:22 -0700

    add contact form

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,19 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 // import Image from "../components/image"
 import SEO from "../components/seo"
-
+console.log(process.env.GATSBY_BASE_URL)
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <form>Form here</form>
-    <Link to="about/">About Us</Link>
+    <ul>
+      <li>
+        <Link to="about/">About Us</Link>
+      </li>
+      <li>
+        <Link to="contact/">Contact</Link>
+      </li>
+    </ul>
   </Layout>
 )
 
